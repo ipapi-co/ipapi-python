5d62d1b173abe8dcf042565e3dc0a4ee1d88dc46
ipapi-co <hello@ipapi.co>
2017-02-15 12:08:15 +0530

    use [security] package for Python 2 only

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
 
 from setuptools import setup
+import sys
+
+if sys.version_info >= (3,2):
+    install_requires = ['requests']
+else:
+    install_requires = ['requests[security]']
 
 setup(
   name = 'ipapi',
   packages = ['ipapi'], 
-  version = '0.4.2',
-  description = 'Python bindings for ipapi (IP address geolocation API - https://ipapi.com)',
+  version = '0.5.2',
+  description = 'Python bindings for ipapi (IP address to location mapping service. Free & paid API for a secure, fast & reliable IP lookup (city, country, latitude, longitude, timezone) - https://ipapi.co)',
   author = 'ipapi',
   license="MIT License",
   author_email = 'pweb@ipapi.co',
   url = 'https://github.com/ipapi-co/ipapi-python', 
-  download_url = 'https://github.com/ipapi-co/ipapi-python/archive/0.4.2.tar.gz', 
-  keywords = ['geolocation', 'location', 'ipapi', 'ipapi.co', 'ip address to location', 'ipaddress', 'ipv4', 'ipv6', 'ip address', 'ip checker', 'ip lookup'], 
+  download_url = 'https://github.com/ipapi-co/ipapi-python/archive/0.5.2.tar.gz', 
+  keywords = ['geolocation', 'ip address', 'ip address geolocation', 'ip address lookup', 'ip address to location', 'ip address tracer', 'ip address tracker', 'ip checker', 'ip lookup', 'ip tracker', 'ip-address', 'ipapi', 'ipapi.co', 'ipv4', 'ipv6', 'location', 'lookup ip address', 'trace ip address'], 
   classifiers = [],
-  install_requires = [
-  'requests[security]',
-  ],
+  install_requires = install_requires,
 )
